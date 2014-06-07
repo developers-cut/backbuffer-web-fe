084955db9a27523701d63aef555affdddc21a04a
Thomas Silva <thomas@royalsflush.com>
2014-06-07 19:44:28 +0100

    Build now done using grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,13 +25,34 @@ module.exports = function(grunt) {
                     }
                 },
                 files: {
-                    "index.html": "app/views/index.jade"
+                    'index.html': 'app/views/index.jade'
                 }
             }
+        },
+        compass: {
+            compile: {
+                options: {
+                    sassDir: 'app/stylesheets/',
+                    cssDir: 'public/css/'
+                }
+            }
+        },
+        uglify: {
+            compile: {
+                files: {
+                    'public/js/backbuffer.js': 'app/scripts/*.js'
+                }
+            }
+        },
+        jshint: {
+            lint_devjs: ['app/scripts/*.js'],
         }
     });
 
     // grunt.loadNpmTasks('grunt-emblem');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.registerTask('default', ['jade']);
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jade', 'compass', 'uglify', 'jshint']);
 }
