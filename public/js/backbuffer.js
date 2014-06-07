c885ef79e8f32b876a328388c2168a18b551b773
Thomas Silva <thomas@royalsflush.com>
2014-06-07 17:05:37 +0100

    Edit just... works. Beautifully. Thank you, Ember

--- public/js/backbuffer.js
+++ public/js/backbuffer.js
@@ -12,19 +12,52 @@ Backbuffer.Router.map(function() {
 Backbuffer.Data = DS.Model.extend({
     title: DS.attr('string'),
     description: DS.attr('string'),
-    closed: DS.attr('boolen'),
+    closed: DS.attr('boolean'),
     assigned_to: DS.attr('string')
 });
 
 Backbuffer.IndexRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('data');
-    }
+    },
 });
 
 Backbuffer.DataRoute = Ember.Route.extend({
     model: function(params) {
         return this.store.find('data', params.data_id);
+    }
+});
+
+Backbuffer.DataController = Ember.ObjectController.extend({
+    basicInfoEdit: false,
+    assignedToEdit: false,
+
+    actions: {
+        editBasicInfo: function(cancel) {
+            if (this.get('basicInfoEdit')) {
+                this.set('basicInfoEdit', false);
+            
+                // Some code.
+            } else {
+                this.set('basicInfoEdit', true);
+
+                // Some code.
+            }
+        },
+        editAssignedTo: function(cancel) {
+            if (this.get('assignedToEdit')) {
+                this.set('assignedToEdit', false);
+
+                // Some code.
+            } else {
+                this.set('assignedToEdit', true);
+
+                // Some code.
+            }
+        },
+        changeStatus: function() {
+            this.set('closed', !this.get('closed'));
+        },
     }
 });
 // Fixtures for Data model - Backbuffer.
