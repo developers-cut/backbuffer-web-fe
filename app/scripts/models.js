941587ae31d4440e81c70fa2d8b858bd55533717
Thomas Silva <thomas@royalsflush.com>
2014-06-08 15:30:42 +0100

    Solves issue #20

---
+++ app/scripts/models.js
@@ -0,0 +1,15 @@
+// Main coffee file for Backbuffer.
+
+var Backbuffer = Ember.Application.create();
+Backbuffer.Store = DS.Store.extend({
+    adapter: DS.FixtureAdapter
+});
+
+Backbuffer.Data = DS.Model.extend({
+    title: DS.attr('string'),
+    description: DS.attr('string'),
+    closed: DS.attr('boolean'),
+    assigned_to: DS.attr('string')
+});
+
+
